557d02ca882363135cb702eb85ff803d4c1b695f
Damien Finn Michaud <damien.f.michaud@maine.edu>
2024-03-14 03:18:41 -0400

    oops

--- task3.py
+++ task3.py
@@ -1,4 +1,12 @@
 import csv
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+
+# whatever this does
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 def read_tsv_file(file_path): 
@@ -7,18 +15,34 @@ def read_tsv_file(file_path):
         reader = csv.reader(tsv, delimiter='\t')
         next(reader)
         for row in reader:
-            Genre, Title, URL, Lyrics = row
-            genre_to_lyrics.append({'Genre': Genre, 'Title': Title, 'URL': URL, 'Lyrics': Lyrics})
+            Genre, _, _, Lyrics = row
+            
+            # Tokenize lyrics before loading array
+            preprocessed_lyrics = preprocess_lyrics(Lyrics)
+
+            genre_to_lyrics.append({'Genre': Genre, 'Lyrics': Lyrics})
         
         
         return genre_to_lyrics
     
+def preprocess_lyrics(lyrics):
+    
+    lyrics = lyrics.lower()
+    lyrics = lyrics.translated(str.maketrans('', '', string.punctuation))
 
+    words = word_tokenize(lyrics)
+
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+
+    preprocessed_lyrics = ' '.join(words)
+
+    return preprocessed_lyrics
+    
+    
 def main():
     tsv_file_path = "unfiltered_genre_to_lyrics.tsv"
 
     res = read_tsv_file(tsv_file_path)
-    for song in res: 
-        print(song)
 
 main()
