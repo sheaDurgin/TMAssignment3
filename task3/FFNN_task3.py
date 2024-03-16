5e99553869584b944c580ea638d3dbeccc632d53
Damien Finn Michaud <damien.f.michaud@maine.edu>
2024-03-15 23:10:11 -0400

    FFNN running on embeddings - need to optimize

---
+++ task3/FFNN_task3.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+
+
+class FFNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(FFNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+    
+
+
+
