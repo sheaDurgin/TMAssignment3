2e4eb5f0b82dfb45e0d81bba43682f270209c76c
Damien Finn Michaud <damien.f.michaud@maine.edu>
2024-03-17 18:01:34 -0400

    Running on testing data 60%

--- task3/task3.py
+++ task3/task3.py
@@ -3,6 +3,8 @@ import nltk
 import certifi
 import ssl
 import torch
+import os
+import re
 import torch.nn as nn
 import torch.optim as optim
 import random
@@ -86,16 +88,10 @@ def generate_lyric_embeddings(preprocessed_lyrics):
     
     return avg_word_embedding
 
-def train_and_validate(train_loader, val_loader, input_size, num_classes, hidden_size, num_epochs, learning_rate):
 
-    model = FFNN(input_size, hidden_size, num_classes)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
+def train_model(train_loader, model, criterion, optimizer, scheduler, num_epochs):
     for epoch in range(num_epochs):
-        model.train()
+        model.train()  # Set the model to training mode
         for inputs, labels in train_loader:
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -103,24 +99,81 @@ def train_and_validate(train_loader, val_loader, input_size, num_classes, hidden
             loss.backward()
             optimizer.step()
 
-        scheduler.step()
+        scheduler.step()  
+
+    return model
+
+# For both testing on validation and testing data 
+def train_and_test(train_loader, test_loader, input_size, num_classes, hidden_size, num_epochs, learning_rate):
+    seed_value = 42
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
     
+    model = FFNN(input_size, hidden_size, num_classes)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    trained_model = train_model(train_loader, model, criterion, optimizer, scheduler, num_epochs)
+    accuracy = evaluate_model(test_loader, trained_model)
+    
+    return accuracy
+
+
+def get_test_data(dir_path):
+    genre_to_songs = {}
+    for genre in os.listdir(dir_path):
+        genre_dir_path = os.path.join(dir_path, genre)
+        if os.path.isdir(genre_dir_path):
+            genre = genre.lower()
+
+            files = os.listdir(genre_dir_path) 
+            song_paths = [os.path.join(genre_dir_path, file) for file in files]
+            genre_to_songs[genre] = []
+            for song in song_paths:
+                with open(song, 'r') as f:
+                    text = f.read()
+                preprocessed_lyrics = preprocess_lyrics(text)
+                lyric_embeddings = generate_lyric_embeddings(preprocessed_lyrics)
+                genre_to_songs[genre].append(lyric_embeddings)
+    
+    X_test = []
+    y_test = []
+    for genre, songs in genre_to_songs.items():
+        X_test.extend(songs)
+        y_test.extend([genre] * len(songs))
+
+    # Encode labels
+    label_encoder = LabelEncoder()
+    y_test_encoded = label_encoder.fit_transform(y_test)
+    
+    return X_test, y_test_encoded
+
+
+def evaluate_model(data_loader, model):
     model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
-        for inputs, labels in val_loader:
+        for inputs, labels in data_loader:
             outputs = model(inputs)
             _, predicted = torch.max(outputs, 1)
             correct += (predicted == labels).sum().item()
             total += len(labels)  
 
-    accuracy = correct / total
+        accuracy = correct / total
     
     return accuracy
-
+    
 # Permute over hyperparameter setups to determine best combination 
 def tune_hyperparameters(train_loader, val_loader, input_size, num_classes, param_grid):
+    seed_value = 42
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    
     best_accuracy = 0
     best_params = {}
 
@@ -129,34 +182,7 @@ def tune_hyperparameters(train_loader, val_loader, input_size, num_classes, para
         hidden_size = params['hidden_size']
         num_epochs = params['num_epochs']
 
-        model = FFNN(input_size, hidden_size, num_classes)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
-        for epoch in range(num_epochs):
-            model.train()
-            for inputs, labels in train_loader:
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-            scheduler.step()
-        
-        model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for inputs, labels in val_loader:
-                outputs = model(inputs)
-                _, predicted = torch.max(outputs, 1)
-                correct += (predicted == labels).sum().item()
-                total += len(labels)  
-
-        accuracy = correct / total
+        accuracy = train_and_test(train_loader, val_loader, input_size, num_classes, hidden_size, num_epochs, learning_rate)
         
         if accuracy > best_accuracy:
             best_accuracy = accuracy
@@ -166,24 +192,18 @@ def tune_hyperparameters(train_loader, val_loader, input_size, num_classes, para
 
 
 def main():
-    # Seed model 
-    seed_value = 42
-    random.seed(seed_value)
-    np.random.seed(seed_value)
-    torch.manual_seed(seed_value)
 
-    tsv_file_path = "unfiltered_genre_to_lyrics.tsv"
+    tsv_file_path = "genre_to_lyrics.tsv"
     unsplit_data = read_tsv_file(tsv_file_path)
     X_train, X_val, y_train, y_val = split_data(unsplit_data)
 
     # Model parameters
     input_size = len(X_train[0])
-    hidden_size = 128
     num_classes = len(np.unique(y_train)) 
     label_encoder = LabelEncoder()
     y_train_encoded = label_encoder.fit_transform(y_train)
     y_val_encoded = label_encoder.fit_transform(y_val)
-
+    X_test, y_test = get_test_data("Test Songs")
 
     train_dataset = TensorDataset(torch.FloatTensor(np.array(X_train)), torch.LongTensor(y_train_encoded))
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
@@ -191,23 +211,32 @@ def main():
     val_dataset = TensorDataset(torch.FloatTensor(np.array(X_val)), torch.LongTensor(y_val_encoded))
     val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
+    test_dataset = TensorDataset(torch.FloatTensor(np.array(X_test)), torch.LongTensor(y_test))
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    accuracy = train_and_test(train_loader, test_loader, input_size, num_classes, 128, 20, 0.01)
+
+    print(accuracy)
     # Hyperparamters to test
-    param_grid = {
-     'learning_rate': [0.001, 0.01, 0.1],
-     'hidden_size': [64, 128, 256],
-     'num_epochs': [10, 20, 30]  
-    }
+    #param_grid = {
+    # 'learning_rate': [0.001, 0.01, 0.1],
+    # 'hidden_size': [64, 128, 256],
+    # 'num_epochs': [10, 20, 30]  
+    #}
 
     # Hyperparameter tuning
-    best_accuracy, best_params = tune_hyperparameters(train_loader, val_loader, input_size, num_classes, param_grid)
-    print("Best validation accuracy:", best_accuracy)
+    #best_accuracy, best_params = tune_hyperparameters(train_loader, val_loader, input_size, num_classes, param_grid)
+    #print("Best validation accuracy:", best_accuracy)
+    #print("Best params:", best_params)
 
-
+    # {'hidden_size': 128, 'learning_rate': 0.01, 'num_epochs': 20}
     # Slow so maybe just put in manually later 
-    accuracy = train_and_validate(train_loader, val_loader, input_size, num_classes, best_params['hidden_size'], best_params['num_epochs'], best_params['learning_rate'])
+    
+    # accuracy = train_and_validate(train_loader, val_loader, input_size, num_classes, 128, 20, 0.01)
 
     
-    print(accuracy)
+
+
 
 # :-) 
 if __name__ == '__main__':
